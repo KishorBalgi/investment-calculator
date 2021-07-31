04953b7f0572e098c1e4781e52bf6c640ef87409
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-07-31 19:54:41 +0530

    SIP Calc

--- Resources/Script/script.js
+++ Resources/Script/script.js
@@ -1,4 +1,5 @@
 "use strict";
+const root = document.documentElement;
 const selectType = document.querySelector(".select-type");
 const investForm = document.querySelector(".invest-form");
 const investStudent = document.querySelector(".invest-student");
@@ -26,28 +27,37 @@ const resultReturnBox = document.querySelector(".result-return");
 const resultReturn = document.querySelector(".result-return p");
 const btnResetReturn = document.querySelector(".reset-return");
 const btnTheme = document.getElementById("btn-theme");
+const SIPInitial = document.getElementById("sip-initial");
+const SIPIntrest = document.getElementById("sip-intrest-slider");
+const SIPIntrestInput = document.getElementById("sip-intrest");
+const SIPPeriod = document.getElementById("sip-period-slider");
+const SIPPeriodVal = document.querySelector(".sip-period-value");
+const SIPResInit = document.querySelector(".sip-result-initial");
+const SIPResRet = document.querySelector(".sip-result-return");
+const SIPResTot = document.querySelector(".sip-result-total");
+let SIPMode = 1;
 // Currency:
 const date = new Date();
 // console.log(`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
 // const currencyVal = fetch(
 //   `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${date.getFullYear()}-${date.getMonth()}-${date.getDate()}/{endpoint}`
 // );
-const currencyVals = fetch(
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    const currencies = Object.keys(data);
-    addCurrencies(currencies);
-  });
-const addCurrencies = function (currencies) {
-  currencies.forEach((cur) => {
-    const html = `<option value="${cur}">${cur.toUpperCase()}</option>`;
-    currency.insertAdjacentHTML("beforeend", html);
-  });
-};
+// const currencyVals = fetch(
+//   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+// )
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     const currencies = Object.keys(data);
+//     addCurrencies(currencies);
+//   });
+// const addCurrencies = function (currencies) {
+//   currencies.forEach((cur) => {
+//     const html = `<option value="${cur}">${cur.toUpperCase()}</option>`;
+//     currency.insertAdjacentHTML("beforeend", html);
+//   });
+// };
 // Submt Invest Working:
 const clearFieldInvest = function () {
   salary.value = age.value = spending.value = saving.value = "";
@@ -85,7 +95,7 @@ const submitInvestWorking = function (e) {
     setErrorInvest("*All fields are required.");
     return;
   }
-  const currencyVal = currency.value.toUpperCase();
+  const currencyVal = "&#8377;";
   const salaryVal = salary.value;
   const ageVal = age.value;
   const spendingVal = spending.value;
@@ -192,3 +202,46 @@ btnTheme.addEventListener("click", function (e) {
     }
   }, 300);
 });
+// SIP Caclculator:
+const changeModeSIP = function (m) {
+  if (m == 1) {
+    document.querySelector(".sip-mode").innerHTML = "Monthly Investment";
+    SIPMode = 1;
+  } else {
+    document.querySelector(".sip-mode").innerHTML = "Total Investment";
+    SIPMode = 2;
+  }
+  renderSIP(document.getElementById(`sip-intrest`).value);
+};
+const renderSIPResult = function (init, int, per) {
+  let maturity;
+  if (SIPMode === 1) {
+    const i = int / 1200;
+    maturity = +(init * (((1 + i) ** (per * 12) - 1) / i) * (1 + i)).toFixed(0);
+    init = init * per * 12;
+  } else {
+    maturity = (init * (1 + int / 100) ** per).toFixed(0);
+  }
+
+  SIPResInit.innerHTML = `&#8377; ${init}`;
+  SIPResRet.innerHTML = `&#8377; ${(maturity - init).toFixed(0)}`;
+  SIPResTot.innerHTML = `&#8377; ${maturity}`;
+  root.style.setProperty("--data-init-sip", `${(init * 100) / maturity}%`);
+};
+const renderSIP = function (int) {
+  SIPIntrest.value = SIPIntrestInput.value = int;
+  const initial = SIPInitial.value;
+  const intrest = SIPIntrest.value;
+  const period = SIPPeriod.value;
+  // Constraints:
+  if (initial > 200000) SIPInitial.value = 200000;
+  if (SIPIntrestInput.value > 30) SIPIntrest.value = SIPIntrestInput.value = 30;
+  SIPPeriodVal.innerHTML = `${period} ${+period === 1 ? "Year" : "Years"}`;
+  // Render Sliders:
+  root.style.setProperty(
+    "--data-intrest-sip",
+    `${(intrest / 0.3).toFixed(2)}%`
+  );
+  root.style.setProperty("--data-period-sip", `${(period / 0.3).toFixed(2)}%`);
+  renderSIPResult(initial, intrest, period);
+};
